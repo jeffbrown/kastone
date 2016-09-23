635ef81adb4399cc41a150d8eddb32f3a1f0e01d
Jeff Scott Brown <brownj@ociweb.com>
2016-09-23 15:42:20 -0500

    add problematic line (commented out)

--- grails-app/controllers/demo/DemoController.groovy
+++ grails-app/controllers/demo/DemoController.groovy
@@ -4,6 +4,10 @@ import org.springframework.http.HttpStatus
 
 class DemoController {
 
+    // this line breaks something...
+    // the model variables in the view don't get populated
+    //static responseFormats = ['json']
+
     def index() {
         respond([firstName: 'Jeff', submissionPackageId: 42l], status: HttpStatus.CREATED)
     }
