943c696c77cb0ffe28cc7281448c1eeae19fca1c
Jeff Scott Brown <brownj@ociweb.com>
2016-10-14 13:32:57 -0500

    Uncomment code that used to be problematic

--- grails-app/controllers/demo/DemoController.groovy
+++ grails-app/controllers/demo/DemoController.groovy
@@ -6,7 +6,7 @@ class DemoController {
 
     // this line breaks something...
     // the model variables in the view don't get populated
-    //static responseFormats = ['json']
+    static responseFormats = ['json']
 
     def index() {
         respond([firstName: 'Jeff', submissionPackageId: 42l], status: HttpStatus.CREATED)
